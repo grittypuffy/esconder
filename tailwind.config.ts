86ccfbfd1c5eeee4d9f91bbbe615ac6b2bcd9c99
grittypuffy <grittypuffy@riseup.net>
2024-11-08 22:35:27 +0530

    Add: Fonts and initial boilerplate for CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,37 @@
-/** @type {import('tailwindcss').Config} */
-
+import preline from "preline/plugin"
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    'index.html',
-    './src/**/*.{js,ts,jsx,tsx,mdx,html}',
-  ],
-  theme: {
-    extend: {
+    content: [
+      'node_modules/preline/dist/*.js',
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx,mdx,html}',
+    ],
+    theme: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        oswald: ["Oswald", "sans-serif"],
+        sourcesans: ["Source Sans", "sans-serif"],
+        'sans': ["Source Sans", "Montserrat", 'system-ui'],
+        'serif': ['ui-serif'],
+        'mono': ['ui-monospace'],
+        'display': ["Source Sans"],
+        'body': ["Source Sans"]
+      },
+      extend: {
+      fontFamily: {
+        regular: ["Oswald"]
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      }
     },
   },
-  plugins: [
-  ],
+  plugins: [  
+    preline
+  ]
 };
 
 export default config;
